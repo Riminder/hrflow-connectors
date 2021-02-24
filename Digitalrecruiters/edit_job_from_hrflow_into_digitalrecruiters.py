18cd8177f5d4312f7e5eaf33c48eecbe9cb2b8c4
Ala Eddine Chhima <alaeddine.chhima@riminder.net>
2021-02-24 16:01:16 +0100

    ADD README.md to Digitalrecruiters connector

---
+++ Digitalrecruiters/edit_job_from_hrflow_into_digitalrecruiters.py
@@ -0,0 +1,11 @@
+from hrflow import Hrflow
+import requests
+
+
+def workflow(settings: dict) -> None:
+    """
+    PULL WORKFLOW allows you to run the following code instructions on a regular basis
+
+    @rtype: None
+    @param settings: dictionary of settings params of the workflow
+    """
