04525864ac84f4d6fda57ee1a9c633bbc8be242b
Corentin Duchêne <corentin.duchene@riminder.net>
2021-12-01 15:20:24 +0100

    feat: add HTTPAction

---
+++ tests/core/test_http.py
@@ -0,0 +1,22 @@
+import requests
+
+from hrflow_connectors.core.http import HTTPAction
+from hrflow_connectors.core.auth import Auth, NoAuth
+
+def test_HTTPAction_get():
+    class TestHTTPAction(HTTPAction):
+        def __init__(self, auth:Auth = NoAuth()):
+            super(TestHTTPAction, self).__init__(auth)
+            self.http_method = "GET"
+            self.url = "https://gorest.co.in/public/v1/posts"
+    
+    action = TestHTTPAction()
+    response = action.execute()
+
+    assert isinstance(response, requests.Response)
+    assert response.status_code == 200
+    assert "application/json" in response.headers.get('content-type')
+
+    response_json = response.json()
+    assert response_json.get("meta") != None
+    assert response_json.get("data") != None
