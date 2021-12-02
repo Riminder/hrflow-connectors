fb49d342e68b2148ffe5f88834e49713d4a55217
Corentin Duchêne <corentin.duchene@riminder.net>
2021-12-02 15:30:22 +0100

    feat: use fixture in crosstalent tests

--- tests/connectors/crosstalent/test_get_all_jobs.py
+++ tests/connectors/crosstalent/test_get_all_jobs.py
@@ -1,5 +1,7 @@
 import os
 import json
+import pytest
+
 from hrflow_connectors.core.auth import OAuth2PasswordCredentialsBody
 from hrflow_connectors.connectors.boards.crosstalent.actions import GetAllJobs
 
@@ -8,26 +10,28 @@ import hrflow_connectors as hc
 ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(hc.__file__), "../../"))
 
 
-def get_credentials():
+@pytest.fixture
+def auth():
     with open(os.path.join(ROOT_PATH, "credentials.json"), "r") as f:
         credentials = json.loads(f.read())
-    return credentials["crosstalent"]["oauth2"]
 
-
-def test_GetAllJobs():
-    credentials = get_credentials()
     access_token_url = "https://test.salesforce.com/services/oauth2/token"
 
     auth = OAuth2PasswordCredentialsBody(
         access_token_url=access_token_url,
-        client_id=credentials["client_id"],
-        client_secret=credentials["client_secret"],
-        username=credentials["username"],
-        password=credentials["password"]
+        client_id=credentials["crosstalent"]["oauth2"]["client_id"],
+        client_secret=credentials["crosstalent"]["oauth2"]["client_secret"],
+        username=credentials["crosstalent"]["oauth2"]["username"],
+        password=credentials["crosstalent"]["oauth2"]["password"]
     )
+    return auth
 
+def test_Auth(auth):
+    assert isinstance(auth.access_token, str)
+    assert auth.access_token != ""
+
+def test_GetAllJobs(auth):
     action = GetAllJobs(auth=auth)
     response = action.execute()
-
+    
     assert response.status_code == 200
-
