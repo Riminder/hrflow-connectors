e060e365572f8d520bdae5b27cce7765a5fb2caa
Corentin Duchêne <corentin.duchene@riminder.net>
2021-12-03 17:36:17 +0100

    feat: inherit GetAllJobs Action from BoardAction

--- src/hrflow_connectors/connectors/boards/crosstalent/actions.py
+++ src/hrflow_connectors/connectors/boards/crosstalent/actions.py
@@ -1,14 +1,15 @@
 from ....core.auth import OAuth2PasswordCredentialsBody
 from ....core.http import HTTPStream
+from ....core.action import BoardAction
 
 
-class GetAllJobs(HTTPStream):
+class GetAllJobs(HTTPStream, BoardAction):
     auth: OAuth2PasswordCredentialsBody
 
     @property
     def url_base(self):
         return "https://vulcain-eng--recette.my.salesforce.com/services/apexrest/crta/HrFlowGetJobOffers/"
-    
+
     @property
     def http_method(self):
         return "GET"
--- src/hrflow_connectors/core/action.py
+++ src/hrflow_connectors/core/action.py
@@ -2,21 +2,32 @@ from pydantic import BaseModel, Field
 from typing import List, Dict, Any, Iterator
 from hrflow import Hrflow
 
+
 class Action(BaseModel):
-    logics: List[str] = Field([], description="Function names to apply as filter before pushing the data")
+    logics: List[str] = Field(
+        [], description="Function names to apply as filter before pushing the data"
+    )
     global_scope: Dict[str, Any] = None
     local_scope: Dict[str, Any] = None
 
-    workflow_catch: bool = Field(True, const=True, description="Indicates if the action is executable in a workflow catch")
-    workflow_pull: bool = Field(True, const=True, description="Indicates if the action is executable in a workflow pull")
+    workflow_catch: bool = Field(
+        True,
+        const=True,
+        description="Indicates if the action is executable in a workflow catch",
+    )
+    workflow_pull: bool = Field(
+        True,
+        const=True,
+        description="Indicates if the action is executable in a workflow pull",
+    )
 
-    def pull(self) ->  Iterator[Dict[str, Any]]:
+    def pull(self) -> Iterator[Dict[str, Any]]:
         """
         Pull data
         """
         raise NotImplementedError("`pull` is not implemented")
-    
-    def apply_logics(self, data : Iterator[Dict[str, Any]]) -> Iterator[Dict[str, Any]]:
+
+    def apply_logics(self, data: Iterator[Dict[str, Any]]) -> Iterator[Dict[str, Any]]:
         """
         Apply filters defined in `logics` on the `data` stream
 
@@ -28,15 +39,17 @@ class Action(BaseModel):
         """
         filtered_list = data
         for logic_function_name in self.logics:
-            logic_function = eval(logic_function_name, self.global_scope, self.local_scope)
+            logic_function = eval(
+                logic_function_name, self.global_scope, self.local_scope
+            )
             filtered_list = filter(logic_function, filtered_list)
         return filtered_list
-    
-    def format(self, data : Dict[str, Any]) -> Dict[str, Any]:
+
+    def format(self, data: Dict[str, Any]) -> Dict[str, Any]:
         """
-        Format `data` fields to another field format. 
-        
-        For example to select and transform only some fields from a database to Hrflow. 
+        Format `data` fields to another field format.
+
+        For example to select and transform only some fields from a database to Hrflow.
         This function must adapt the data schema passed in (from the `pull`) to the expected data schema in output (ready to be used in the `pull` function)
 
         Args:
@@ -47,7 +60,7 @@ class Action(BaseModel):
         """
         return data
 
-    def push(self, data : Iterator[Dict[str, Any]]):
+    def push(self, data: Iterator[Dict[str, Any]]):
         """
         Push data
 
@@ -55,7 +68,7 @@ class Action(BaseModel):
             data (List[Dict[str, Any]]): Data to push
         """
         raise NotImplementedError("`push` is not implemented")
-    
+
     def execute(self):
         """
         Execute action
@@ -63,9 +76,9 @@ class Action(BaseModel):
         input_data = self.pull()
 
         filtered_data = self.apply_logics(input_data)
-        
+
         # connect each filtered_data to the format accepted by the pull function (destination, source, board)
-        output_data = map(self.format, filtered_data)        
+        output_data = map(self.format, filtered_data)
         self.push(output_data)
 
 
@@ -73,6 +86,12 @@ class BoardAction(Action):
     hrflow_client: Hrflow
     board_key: str
 
-    def push(self, data : Iterator[Dict[str, Any]]):
+    class Config:
+        # `Hrflow` class is arbitrary type and can not be use without this option
+        arbitrary_types_allowed = True
+
+    def push(self, data: Iterator[Dict[str, Any]]):
         for job in data:
-            self.hrflow_client.job.indexing.add_json(board_key=self.board_key, job_json=job)
+            self.hrflow_client.job.indexing.add_json(
+                board_key=self.board_key, job_json=job
+            )
