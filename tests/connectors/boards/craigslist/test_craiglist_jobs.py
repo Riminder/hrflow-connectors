66ed80e175b9b08aaf78aae5002cefed24830a53
Corentin Duchêne <corentin.duchene@epita.fr>
2021-12-30 11:59:09 +0100

    fix: replace `ROOT_PATH` by `pytestconfig`

--- tests/connectors/boards/craigslist/test_craiglist_jobs.py
+++ tests/connectors/boards/craigslist/test_craiglist_jobs.py
@@ -3,19 +3,16 @@ import json
 import pytest
 from hrflow import Hrflow
 
-import hrflow_connectors as hc
 from hrflow_connectors.connectors.boards.craigslist.actions import CraigslistJobs
 from hrflow_connectors.utils.logger import get_logger_with_basic_config
 
 # Adding web driver manager as a DEV dependency to make testing easier for users
 from webdriver_manager.chrome import ChromeDriverManager
 
-ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(hc.__file__), "../../"))
-
 
 @pytest.fixture
-def credentials():
-    with open(os.path.join(ROOT_PATH, "credentials.json"), "r") as f:
+def credentials(pytestconfig):
+    with open(os.path.join(pytestconfig.rootpath, "credentials.json"), "r") as f:
         credentials = json.loads(f.read())
     return credentials
 
--- tests/connectors/boards/smartrecruiters/test_smart_jobs.py
+++ tests/connectors/boards/smartrecruiters/test_smart_jobs.py
@@ -2,17 +2,14 @@ import os
 import json
 import pytest
 from hrflow import Hrflow
-import hrflow_connectors as hc
 from hrflow_connectors.core.auth import SmartToken
 from hrflow_connectors.connectors.boards.smartrecruiters.actions import SmartJobs
 from hrflow_connectors.utils.logger import get_logger_with_basic_config
 
-ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(hc.__file__), "../../"))
-
 
 @pytest.fixture
-def credentials():
-    with open(os.path.join(ROOT_PATH, "credentials.json"), "r") as f:
+def credentials(pytestconfig):
+    with open(os.path.join(pytestconfig.rootpath, "credentials.json"), "r") as f:
         credentials = json.loads(f.read())
     return credentials
 
@@ -51,6 +48,6 @@ def test_SmartJobs(auth, hrflow_client):
         board_key="8ebdea98768dfc04d15f76afab70415ed280ea90",
         hydrate_with_parsing=False,
         archive_deleted_jobs_from_stream=False,
-        posting_status="PUBLIC"
+        posting_status="PUBLIC",
     )
     action.execute()
