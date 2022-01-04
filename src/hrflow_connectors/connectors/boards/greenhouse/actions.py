72669d239b172ef690f3699d6cf50bec762700db
Limam Vadhel <limam.vadhel@hrflow.ai>
2022-01-04 15:28:04 +0100

    style: reformat with black

--- src/hrflow_connectors/connectors/boards/greenhouse/actions.py
+++ src/hrflow_connectors/connectors/boards/greenhouse/actions.py
@@ -1,6 +1,6 @@
 from typing import Iterator, Dict, Any
 from pydantic import Field
-from html.parser import HTMLParser
+import html
 from ....core.action import BoardAction
 from ....core.http import HTTPStream
 from ....utils.logger import get_logger
@@ -12,7 +12,7 @@ logger = get_logger()
 class GetAllJobs(HTTPStream, BoardAction):
     board_token: str = Field(
         ...,
-        description="Job Board URL token, which is usually the company `name` -for example `lyft`- when it has job listings on greenhouse, mandatory to access job boards on `greenhouse.io`: `https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs` getting jobs doesn't require an API Key",
+        description="Job Board URL token, which is usually the company `name` -for example `lyft`- when it has job listings on greenhouse, mandatory to access job boards on `greenhouse.io`: `https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs`, getting jobs doesn't require an API Key",
     )
 
     @property
@@ -30,8 +30,6 @@ class GetAllJobs(HTTPStream, BoardAction):
     def pull(self) -> Iterator[Dict[str, Any]]:
         """pull : sends a request to get all jobs from a greenhouse job board
 
-        
-
         Returns:
             Iterator[Dict[str, Any]]: list of all jobs with their content if available
         """
@@ -39,13 +37,14 @@ class GetAllJobs(HTTPStream, BoardAction):
         response = self.send_request()
         if response.status_code == 200:
             job_dict = response.json()
-            total_info= job_dict["meta"]["total"]
+            total_info = job_dict["meta"]["total"]
             job_list = job_dict["jobs"]
             job_number = len(job_list)
             logger.info(f"{job_number} job(s) got. Total found : {total_info}")
             return job_list
         else:
             logger.error(f"Failed to get jobs from board: {self.board_token}")
+            logger.debug(f"Check that your board token: {self.board_token} is valid")
             error_message = "Unable to pull the data ! Reason : `{}`"
             raise ConnectionError(error_message.format(response.content))
 
@@ -65,14 +64,16 @@ class GetAllJobs(HTTPStream, BoardAction):
         job["location"] = dict(text=location, lat=None, lng=None)
 
         description_content = data.get("content")
-        parser = HTMLParser()
-        text = remove_html_tags(parser.unescape(description_content))
 
-        job["sections"] = [dict(
-            name="greenhouse_description",
-            title="greenhouse_description",
-            description=text,
-        )]
+        text = remove_html_tags(html.unescape(description_content))
+
+        job["sections"] = [
+            dict(
+                name="greenhouse_description",
+                title="greenhouse_description",
+                description=text,
+            )
+        ]
         job["metadata"] = data.get("metadata")
 
         department_name = data.get("departments")
