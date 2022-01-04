876735229155eadd421a22466fec0b88d1fc0bee
Limam Vadhel <limam.vadhel@hrflow.ai>
2022-01-04 15:20:19 +0100

    feat: add `HTMLParser` and `remove_html_tags` for description

--- src/hrflow_connectors/connectors/boards/greenhouse/actions.py
+++ src/hrflow_connectors/connectors/boards/greenhouse/actions.py
@@ -1,26 +1,26 @@
 from typing import Iterator, Dict, Any
 from pydantic import Field
-
+from html.parser import HTMLParser
 from ....core.action import BoardAction
 from ....core.http import HTTPStream
-from ....core.auth import NoAuth
 from ....utils.logger import get_logger
-
+from ....utils.clean_text import remove_html_tags
 
 logger = get_logger()
 
 
 class GetAllJobs(HTTPStream, BoardAction):
-    auth: NoAuth
-    BOARD_TOKEN: str = Field(
+    board_token: str = Field(
         ...,
-        description="Job Board URL token, mandatory to access job boards on `greenhouse.io`: `https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs` getting jobs doesn't require an API Key",
+        description="Job Board URL token, which is usually the company `name` -for example `lyft`- when it has job listings on greenhouse, mandatory to access job boards on `greenhouse.io`: `https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs` getting jobs doesn't require an API Key",
     )
 
     @property
     def base_url(self):
-        return "https://boards-api.greenhouse.io/v1/boards/{}/jobs".format(
-            self.BOARD_TOKEN
+        return (
+            "https://boards-api.greenhouse.io/v1/boards/{}/jobs/?content=true".format(
+                self.board_token
+            )
         )
 
     @property
@@ -28,34 +28,65 @@ class GetAllJobs(HTTPStream, BoardAction):
         return "GET"
 
     def pull(self) -> Iterator[Dict[str, Any]]:
+        """pull : sends a request to get all jobs from a greenhouse job board
 
-        self.params["content"] = True
+        
+
+        Returns:
+            Iterator[Dict[str, Any]]: list of all jobs with their content if available
+        """
+
         response = self.send_request()
         if response.status_code == 200:
             job_dict = response.json()
-            TOTAL_JOB = job_dict["meta"]["total"]
-            logger.info(f"total jobs found: {TOTAL_JOB}")
+            total_info= job_dict["meta"]["total"]
             job_list = job_dict["jobs"]
+            job_number = len(job_list)
+            logger.info(f"{job_number} job(s) got. Total found : {total_info}")
             return job_list
         else:
-            logger.error(f"Failed to get jobs from board: {self.BOARD_TOKEN}")
+            logger.error(f"Failed to get jobs from board: {self.board_token}")
             error_message = "Unable to pull the data ! Reason : `{}`"
             raise ConnectionError(error_message.format(response.content))
 
     def format(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        """format : convert every job pulled from greenhouse job board into a HrFlow job object
+
+        Returns:
+            Dict[str, Any]: job in the HrFlow job object format
+        """
 
         job = dict()
-
         job["name"] = data.get("title")
-        job["reference"] = data.get("internal_job_id")
+        job["summary"] = None
+        job["reference"] = str(data.get("internal_job_id"))
         job["url"] = data.get("absolute_url")
         location = data.get("location").get("name")
         job["location"] = dict(text=location, lat=None, lng=None)
-        description = data.get("content")
-        job["sections"] = dict(
+
+        description_content = data.get("content")
+        parser = HTMLParser()
+        text = remove_html_tags(parser.unescape(description_content))
+
+        job["sections"] = [dict(
             name="greenhouse_description",
             title="greenhouse_description",
-            description=description,
-        )
+            description=text,
+        )]
+        job["metadata"] = data.get("metadata")
+
+        department_name = data.get("departments")
+        office_name = data.get("offices")
+        education = data.get("education_optional")
+        employment = data.get("employment")
+
+        job["tags"] = [
+            dict(name="greenhouse_department", value=department_name),
+            dict(name="greenhouse_office", value=office_name),
+            dict(name="greenhouse_education", value=education),
+            dict(name="greenhouse_employment", value=employment),
+        ]
+
+        job["updated_at"] = data.get("updated_at")
 
         return job
