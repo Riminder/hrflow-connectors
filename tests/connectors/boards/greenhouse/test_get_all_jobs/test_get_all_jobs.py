e7b1d86c0e6c940c3b0eaaf414e65153be4897b8
Limam Vadhel <limam.vadhel@hrflow.ai>
2022-01-04 15:20:19 +0100

    feat: add `HTMLParser` and `remove_html_tags` for description

---
+++ tests/connectors/boards/greenhouse/test_get_all_jobs/test_get_all_jobs.py
@@ -0,0 +1,12 @@
+
+from hrflow_connectors.connectors.boards.greenhouse import GetAllJobs
+
+
+def test_GetAllJobs(logger, hrflow_client):
+    action = GetAllJobs(
+        board_token='lyft',
+        hrflow_client=hrflow_client("dev-demo"),
+        board_key="fc48d01539cde9e8a8ca17238a61c16826837729",
+        hydrate_with_parsing=True,
+    )
+    action.execute()
