2b7e10e80d43d457aa9359b7d60791cc493dc6e1
Limam Vadhel <limam.vadhel@hrflow.ai>
2022-01-04 15:28:04 +0100

    style: reformat with black

--- tests/connectors/boards/greenhouse/test_get_all_jobs/test_get_all_jobs.py
+++ tests/connectors/boards/greenhouse/test_get_all_jobs/test_get_all_jobs.py
@@ -4,7 +4,7 @@ from hrflow_connectors.connectors.boards.greenhouse import GetAllJobs
 
 def test_GetAllJobs(logger, hrflow_client):
     action = GetAllJobs(
-        board_token='lyft',
+        board_token='vaulttec',
         hrflow_client=hrflow_client("dev-demo"),
         board_key="fc48d01539cde9e8a8ca17238a61c16826837729",
         hydrate_with_parsing=True,
