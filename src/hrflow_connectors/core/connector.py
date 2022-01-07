dc39ac678235df4360e59ef95dc10350bad133d3
Corentin Duchêne <corentin.duchene@riminder.net>
2022-01-07 15:27:19 +0100

    docs: add basic doc for `Connector`

--- src/hrflow_connectors/core/connector.py
+++ src/hrflow_connectors/core/connector.py
@@ -1,12 +1,28 @@
 class Connector:
+    """
+    Abstract class `Connector`
+    """
+
     def pull_jobs(self, *args, **kwargs):
+        """
+        Pull jobs
+        """
         raise NotImplementedError("`pull_jobs` method is not implemented !")
 
     def pull_profiles(self, *args, **kwargs):
+        """
+        Pull profiles
+        """
         raise NotImplementedError("`pull_profiles` method is not implemented !")
 
     def push_job(self, *args, **kwargs):
+        """
+        Push job
+        """
         raise NotImplementedError("`push_job` method is not implemented !")
 
     def push_profile(self, *args, **kwargs):
+        """
+        Push profile
+        """
         raise NotImplementedError("`push_profile` method is not implemented !")
