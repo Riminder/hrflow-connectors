a8ff97d7e03af4a95d2b2d3850979ffcffe926a2
Corentin Duchêne <corentin.duchene@riminder.net>
2022-01-12 10:45:19 +0100

    test: `Connector` interface to call Crosstalent

--- tests/connectors/crosstalent/test_pull_jobs.py
+++ tests/connectors/crosstalent/test_pull_jobs.py
@@ -1,7 +1,7 @@
 import pytest
 
 from hrflow_connectors.core.auth import OAuth2PasswordCredentialsBody
-from hrflow_connectors.connectors.crosstalent import PullJobsAction
+from hrflow_connectors.connectors.crosstalent import Crosstalent
 
 
 @pytest.fixture
@@ -18,18 +18,11 @@ def auth(credentials):
     return auth
 
 
-def test_Auth(auth):
-    access_token = auth.get_access_token()
-    assert isinstance(access_token, str)
-    assert access_token != ""
-
-
 def test_PullJobsAction(logger, auth, hrflow_client):
-    action = PullJobsAction(
+    Crosstalent.pull_jobs(
         auth=auth,
         subdomain="vulcain-eng--recette.my",
         hrflow_client=hrflow_client("dev-demo"),
         board_key="8eba188e1af123a9818d00974ff37b943b7d54f4",
         hydrate_with_parsing=True,
     )
-    action.execute()
