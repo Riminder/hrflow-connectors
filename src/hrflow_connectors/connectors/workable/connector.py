9e1468b1d6cac7f6e4e962894e411b800aa714da
limamvadhel <limam.vadhel@hrflow.ai>
2022-01-13 15:29:50 +0100

    update: adapt workable connector

---
+++ src/hrflow_connectors/connectors/workable/connector.py
@@ -0,0 +1,20 @@
+from hrflow import Hrflow
+from typing import Optional, Dict, Any
+
+from ...core.connector import Connector
+from .actions import PullJobsAction
+
+
+class Workable:
+    @staticmethod
+    def pull_jobs(
+        hrflow_client: Hrflow, board_key: str, subdomain: str, **kwargs
+    ) -> Optional[Dict[str, Any]]:
+
+        action = PullJobsAction(
+            hrflow_client=hrflow_client,
+            board_key=board_key,
+            subdomain=subdomain,
+            **kwargs
+        )
+        return action.execute()
