b4d9ed46c2938d4652e0ac3d58b9c7bcd9b167ad
clement <clement.negre@riminder.net>
2022-01-17 11:39:23 +0100

    feat: add test action

--- tests/core/test_action.py
+++ tests/core/test_action.py
@@ -4,8 +4,9 @@ from hrflow_connectors.core.action import (
     PushAction,
     PullJobsAction,
     PushProfileAction,
+    PushJobAction,
 )
-from hrflow_connectors.utils.hrflow import Profile, Source
+from hrflow_connectors.utils.hrflow import Profile, Source, Job, Board
 import pytest
 import requests
 import responses
@@ -939,7 +940,7 @@ def test_PullJobsAction_push_success(hrflow_client):
 
 
 @responses.activate
-def test_PullJobsAction_pull_failure(hrflow_client):
+def test_PullJobsAction_push_failure(hrflow_client):
     # Mock requests and check data sent
     job = dict(key="efg", reference="REF123")
     expected_body = dict(board_key="abc", **job)
@@ -1036,3 +1037,79 @@ def test_PushProfileAction_execute(hrflow_client):
 
     assert workflow_response["status_code"] == 201
     assert workflow_response["message"] == "Profile successfully pushed"
+
+
+@responses.activate
+def test_PushJobAction_execute(hrflow_client):
+    job = Job(key="efg", board=Board(key="abc"))
+
+    # Mock the `pull` & `push` method to do nothing
+    class MyPushJobAction(PushJobAction):
+        def pull(self):
+            return ["pullformat", "pulllogic"]
+
+        def format(self, data):
+            assert "pull" in data
+            return data.replace("pull", "")
+
+        def push(self, data):
+            data_list = list(data)
+            assert data_list == ["format", "logic"]
+
+    action = MyPushJobAction(hrflow_client=hrflow_client(), job=job)
+
+    workflow_response = action.execute()
+
+    assert workflow_response["status_code"] == 201
+    assert workflow_response["message"] == "Profile successfully pushed"
+
+
+@responses.activate
+def test_PushJobAction_pull_success(hrflow_client):
+    # Mock requests and check data sent
+    expected_params = dict(board_key="abc", key="efg")
+    returned_value = dict(code=200, data=dict(key="efg"))
+    match = [responses.matchers.query_param_matcher(expected_params)]
+    responses.add(
+        responses.GET,
+        "https://api.hrflow.ai/v1/job/indexing?",
+        status=200,
+        match=match,
+        json=returned_value,
+    )
+
+    # Pull data
+    job = Job(key="efg", board=Board(key="abc"))
+    action = PushJobAction(hrflow_client=hrflow_client(), job=job)
+    profile_list_got = action.pull()
+
+    # Check returned value
+    assert len(profile_list_got) == 1
+
+    profile_got = profile_list_got[0]
+    assert profile_got["key"] == "efg"
+
+
+@responses.activate
+def test_PushJobAction_pull_failure(hrflow_client):
+    # Mock requests and check data sent
+    expected_params = dict(board_key="abc", key="efg")
+    returned_value = dict(code=400, message="Test", data=dict())
+    match = [responses.matchers.query_param_matcher(expected_params)]
+    responses.add(
+        responses.GET,
+        "https://api.hrflow.ai/v1/job/indexing",
+        status=400,
+        match=match,
+        json=returned_value,
+    )
+
+    # Pull data
+    job = Job(key="efg", board=Board(key="abc"))
+    action = PushJobAction(hrflow_client=hrflow_client(), job=job)
+
+    try:
+        action.pull()
+        assert False
+    except RuntimeError:
+        pass
