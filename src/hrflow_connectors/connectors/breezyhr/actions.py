2a88d5b985a3903eaa226dc54a1626cd139f6b2b
limamvadhel <limam.vadhel@hrflow.ai>
2022-01-17 14:40:23 +0100

    feat: add breezyhr pull jobs

--- src/hrflow_connectors/connectors/breezyhr/actions.py
+++ src/hrflow_connectors/connectors/breezyhr/actions.py
@@ -12,19 +12,42 @@ logger = get_logger()
 
 class PullJobsAction(core.PullJobsAction):
     auth: OAuth2EmailPasswordBody
-    company_id: int= Field(..., description="ID of company to pull jobs from in Breezy HR database")
+    company_id: Optional[int]= Field(None, description="ID of company to pull jobs from in Breezy HR database associated with the authenticated user")
+    company_name: Optional[str]= Field(None, description="the company associated with the authenticated user")
 
     def pull(self) -> Iterator[Dict[str, Any]]:
         """
         Pull jobs from a Taleez jobs owner endpoint
         Returns list of all jobs that have been pulled
         """
+        def get_company_id():
+            """
+            getting the company id associated with the authenticated user company
+
+            """
+            if self.company_id is not None:
+                return self.company_id
+            else:
+                get_company_id_request = requests.Request()
+                get_company_id_request.method = "GET"
+                get_company_id_request.url = "https://api.breezy.hr/v3/companies"
+                get_company_id_request.auth = self.auth
+                prepared_request = get_company_id_request.prepare()
+                response = session.send(prepared_request)
+                if not response.ok:
+                    error_message = "Couldn't get company id ! Reason : `{}`"
+                    raise RuntimeError(error_message.format(response.content))
+                company_list = response.json()
+                logger.debug("CRetrieving company id")
+                for company in company_list:
+                    if company['name'] == self.company_name:
+                        return company["_id"]
 
         # Prepare request
         session = requests.Session()
         pull_jobs_request = requests.Request()
         pull_jobs_request.method = "GET"
-        pull_jobs_request.url = f"https://api.breezy.hr/v3/company/{self.company_id}/positions?"
+        pull_jobs_request.url = f"https://api.breezy.hr/v3/company/{get_company_id()}/positions?"
         pull_jobs_request.auth = self.auth
         prepared_request = pull_jobs_request.prepare()
 
@@ -41,12 +64,19 @@ class PullJobsAction(core.PullJobsAction):
         return response.json()
 
     def format(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        format a Breezy Hr job object into a hrflow job object
 
+        Returns:
+            Dict[str, Any]: a job object in the hrflow job format
+        """
         job = dict()
 
         # Basic information
         job["name"] = data.get("name")
+        logger.info(job["name"])
         job["reference"] = data.get("_id")
+        logger.info(job["reference"])
         job["summary"] = None
 
         # Location
@@ -55,8 +85,47 @@ class PullJobsAction(core.PullJobsAction):
         country_name = country.get("name")
         city = location.get("city")
         address = location.get("name")
-        gejson = dict(country=country_name, city=city)
+        geojson = dict(country=country_name, city=city)
         
-        job["location"] = dict(text=address, lat=None, lng=None)
+        job["location"] = dict(text=address,geojson=geojson, lat=None, lng=None)
+
+        # Sections
+        description = remove_html_tags(data.get("description"))
+        cleaned_description = description.replace("&nbsp;", " ")
+        job['sections'] = [
+            dict(name="breezy_hr_description", title="Breezy_hr_description", description=cleaned_description)
+        ]
+        #tags
+        job["tags"] = []
+        def create_tag(field_name: str):
+            tag_name = "breezy_hr_{}".format(field_name)
+            tag_value = data.get(field_name)
+
+            if isinstance(tag_value,dict):
+                tag_name_value = tag_value.get('name')
+                tag = dict(name=tag_name, value=tag_name_value)
+                job["tags"].append(tag)
+            if isinstance(tag_value,str):
+                tag = dict(name=tag_name, value=tag_value)
+                job["tags"].append(tag)
+
+        create_tag("type")
+        create_tag("experience")
+        create_tag("education")
+        create_tag("department")
+        create_tag("requisition_id")
+        create_tag("category")
+        create_tag("candidate_type")
+        is_remote = dict(name="breezy_hr_remote", value=location.get("is_remote"))
+        job["tags"].append(is_remote)
+
+        job["created_at"] = data.get("creation_date")
+        job["updated_at"] = data.get("updated_date")
+
+        job["metadatas"] = data.get("tags")
+        return job
+
+        
+        
         
 
