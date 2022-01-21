cad38c8956d78780ff2f8c12d2abc9b49c72b2a3
Corentin Duchêne <corentin.duchene@riminder.net>
2022-01-21 17:56:56 +0100

    feat: add `HrflowError` Exception

--- src/hrflow_connectors/core/error.py
+++ src/hrflow_connectors/core/error.py
@@ -1,6 +1,7 @@
 import requests
 
 from ..utils.logger import get_logger
+from typing import Dict, Any
 
 logger = get_logger()
 
@@ -38,6 +39,7 @@ class ActionError(ConnectorError):
         ... ActionError: Failed to do
         ... job_id: `15`
         ... job_ref: `abc`
+        ... Status code: `400`
         ... Server response: `{message="error"}`
         """
         message = f"{title}\n"
@@ -57,10 +59,12 @@ class PullError(ActionError):
         Args:
             response (requests.Response): server response
 
+        >>> response = requests.get("http://test.test/")
         >>> raise PullError(response, job_id=15, job_ref="abc")
         ... PullError: Failed to pull
         ... job_id: `15`
         ... job_ref: `abc`
+        ... Status code: `400`
         ... Server response: `{message="error"}`
         """
         title = "Failed to pull"
@@ -75,11 +79,49 @@ class PushError(ActionError):
         Args:
             response (requests.Response): server response
 
+        >>> response = requests.post("http://test.test/")
         >>> raise PushError(response, job_id=15, job_ref="abc")
         ... PushError: Failed to push
         ... job_id: `15`
         ... job_ref: `abc`
+        ... Status code: `400`
         ... Server response: `{message="error"}`
         """
         title = "Failed to push"
         super().__init__(response, title, **kwargs)
+
+
+class HrflowError(ConnectorError):
+    """
+    Hrflow Error
+    """
+
+    def __init__(self, response: Dict[str, Any], title: str, **kwargs):
+        """
+        Hrflow Error
+
+        Args:
+            response (Dict[str, Any]): Hrflow request response
+            title (str): title shown as the first error line
+
+        >>> client = hrflow.Hrflow(api_secret="x_api_key", api_user="x_user_email")
+        >>> response = client.profile.indexing.get(...)
+        >>> raise PushError(response, "Get Profile failed !", profile_key="abc", source_key="efg")
+        ... HrflowError: Get Profile failed !
+        ... profile_key: `abc`
+        ... source_key: `efg`
+        ... Status code: `500`
+        ... Hrflow message: `Internal error`
+        """
+        message = f"{title}\n"
+        for field_name, field_value in kwargs.items():
+            message += f"{field_name}: `{field_value}`\n"
+
+        status_code = response["code"]
+        message += f"Status code: `{status_code}`\n"
+
+        hrflow_message = response["message"]
+        message += f"Hrflow message: `{hrflow_message}`"
+
+        logger.error(title)
+        super().__init__(message)
