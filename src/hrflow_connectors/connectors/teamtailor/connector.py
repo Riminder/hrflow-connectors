05c390dd5922bb3c6fdf041943c1c098b0f33bd5
limamvadhel <limam.vadhel@hrflow.ai>
2022-01-24 14:57:32 +0100

    feat: add profile connector for teamtailor

--- src/hrflow_connectors/connectors/teamtailor/connector.py
+++ src/hrflow_connectors/connectors/teamtailor/connector.py
@@ -4,7 +4,7 @@ from typing import Optional, Dict, Any, List
 from ...core.connector import Connector
 from ...core.auth import AuthorizationAuth
 from ...utils.hrflow import Profile
-from .actions import PullJobsAction
+from .actions import PullJobsAction, PushProfileAction
 
 
 class Teamtailor(Connector):
@@ -35,6 +35,35 @@ class Teamtailor(Connector):
             auth=auth,
             hrflow_client=hrflow_client,
             board_key=board_key,
+            **kwargs
+        )
+        return action.execute()
+    @staticmethod
+    def push_profile(
+        auth: AuthorizationAuth, 
+        hrflow_client: Hrflow, 
+        profile: Profile, 
+        **kwargs
+    ) -> Optional[Dict[str, Any]]:
+        """
+        `PushProfileAction` pushes a Hrflow.ai profile to `Teamtailor` via their Teamtailor API.
+        `Hrflow.ai` -> `Teamtailor`
+        Args:
+            hrflow_client (Hrflow): Hrflow client instance used to communicate with the Hrflow.ai API
+            auth (AuthorizationAuth): Auth instance to identify and communicate with the platform
+            profile (Profile): Profile to push
+            sourced (bool): True if added by a recruiter without applying. Default value `False`
+            logics (List[str], optional): Function names to apply as filter before pushing the data. Default value `[]`
+            global_scope (Optional[Dict[str, Any]], optional): A dictionary containing the current scope's global variables. Default value `None`
+            local_scope (Optional[Dict[str, Any]], optional): A dictionary containing the current scope's local variables. Default value `None`
+            format_function_name (Optional[str], optional): Function name to format job before pushing. Default value `None`
+        Returns:
+            Optional[Dict[str, Any]]: Workflow response or `None`
+        """
+        action = PushProfileAction(
+            auth=auth,
+            hrflow_client=hrflow_client,
+            profile=profile,
             **kwargs
         )
         return action.execute()
