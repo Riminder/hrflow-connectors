2896e73399d006867191b8e8e6c45f8dbcbd1d9b
limamvadhel <limam.vadhel@student-cs.fr>
2022-01-26 10:52:01 +0100

    feat: add schemas for crosstalent push profile model

--- src/hrflow_connectors/connectors/crosstalent/schemas.py
+++ src/hrflow_connectors/connectors/crosstalent/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
 
 class CrosstalentJob(BaseModel):
@@ -141,3 +141,177 @@ class CrosstalentJob(BaseModel):
     crtarecr__Nb_of_applications_still_to_be_processed__c: Optional[int]
     Besoin_client__c: Optional[str]
     crta__Entite__c: Optional[str]  
+
+class FieldLocation(BaseModel):
+    """ Location for profile info, experience and education information"""
+
+    text: Optional[str] = Field(None, description="Location text address.")
+    lat: Optional[float] = Field(
+        None, description="Geocentric latitude of the Location."
+    )
+    lng: Optional[float] = Field(
+        None, description="Geocentric longitude of the Location."
+    )
+    Fields: Optional[Dict[str, Any]] = Field(
+        None, description="other location attributes like country, country_codeNoneetc"
+    )
+
+
+class FieldSkill(BaseModel):
+    name: str = Field(..., description="Identification name of the skill")
+    type: str = Field(..., description="Type of the skill. hard or soft")
+    value: Optional[str] = Field(None, description="Value associated to the skill")
+
+
+class InfoUrls(BaseModel):
+    """ Porfile urls """
+
+    from_resume: Optional[List[str]]
+    linkedin: Optional[str]
+    twitter: Optional[str]
+    facebook: Optional[str]
+    github: Optional[str]
+
+
+class HrflowProfileField(BaseModel):
+    """
+    HrflowJobField: Languages, Certifications, Courses, Tasks, Interests, Metadatas, Tags
+    are arrays of objects following the JSON structure below
+    """
+
+    name: str = Field(..., description="Identification name of the Object")
+    value: Optional[str] = Field(
+        None, description="Value associated to the Object's name"
+    )
+
+
+class CrosstalentProfileInfo(BaseModel):
+    """ Profile basic informations"""
+
+    full_name: Optional[str]
+    first_name: Optional[str]
+    last_name: str
+    email: str
+    phone: Optional[str]
+    date_birth: Optional[str] = Field(None, description="Profile date of birth")
+    location: Optional[FieldLocation] = Field(
+        None, description="Profile location object"
+    )
+    urls: Optional[InfoUrls] = Field(
+        None, description="Profile social networks and URLs"
+    )
+    picture: Optional[str] = Field(None, description="Profile picture url")
+    gender: Optional[str] = Field(None, description="Profile gender")
+    summary: Optional[str] = Field(None, description="Profile summary text")
+
+
+class Experience(BaseModel):
+    key: Optional[str] = Field(None, description="Identification key of the Experience.")
+    company: Optional[str] = Field(None, description="Company name of the Experience.")
+    title: Optional[str] = Field(None, description="Title of the Experience.")
+    description: Optional[str] = Field(
+        None, description="Description of the Experience."
+    )
+    location: Optional[FieldLocation] = Field(
+        None, description="Location object of the Experience."
+    )
+    date_start: Optional[str] = Field(
+        None, description="Start date of the experience. type: ('datetime ISO 8601')"
+    )
+    date_end: Optional[str] = Field(
+        None, description="End date of the experience. type: ('datetime ISO 8601')"
+    )
+    skills: Optional[List[FieldSkill]] = Field(
+        None, description="List of skills of the Experience."
+    )
+    certifications: Optional[List[HrflowProfileField]]
+    courses: Optional[List[HrflowProfileField]]
+    tasks: Optional[List[HrflowProfileField]]
+
+
+class Education(BaseModel):
+    key: Optional[str] = Field(None, description="Identification key of the Education.")
+    school: Optional[str] = Field(None, description="School name of the Education.")
+    title: Optional[str] = Field(None, description="Title of the Education.")
+    description: Optional[str] = Field(None, description="Description of the Education.")
+    location: Optional[FieldLocation] = Field(
+        None, description="Location object of the Education."
+    )
+    date_start: Optional[str] = Field(
+        None, description="Start date of the Education. type: ('datetime ISO 8601')"
+    )
+    date_end: Optional[str] = Field(
+        None, description="End date of the Education. type: ('datetime ISO 8601')"
+    )
+    skills: Optional[List[FieldSkill]] = Field(
+        None, description="List of skills of the Education."
+    )
+    certifications: Optional[List[HrflowProfileField]]
+    courses: Optional[List[HrflowProfileField]]
+    tasks: Optional[List[HrflowProfileField]]
+
+
+class CrosstalentProfile(BaseModel):
+    """ Crosstalent Push Profile object model"""
+
+    key: Optional[str] = Field(None, description="Identification key of the Profile.")
+    reference: Optional[str] = Field(None, description="Custom identifier of the Profile.")
+    archieved_at: Optional[str] = Field(
+        None,
+        description="type: datetime ISO8601, Archive date of the Profile. The value is null for unarchived Profiles.",
+    )
+    updated_at: Optional[str] = Field(
+        None, description="type: datetime ISO8601, Last update date of the Profile."
+    )
+    created_at: Optional[str] = Field(
+        None, description="type: datetime ISO8601, Creation date of the Profile."
+    )
+    info: CrosstalentProfileInfo = Field(...
+    ,description="Object containing the Profile's info."
+    )
+    text_language: str = Field(
+        ..., description="Code language of the Profile. type: string code ISO 639-1"
+    )
+    text: str = Field(..., description="Full text of the Profile..")
+    experiences_duration: float = Field(
+        ..., description="Total number of years of experience."
+    )
+    educations_duration: float = Field(
+        ..., description="Total number of years of education."
+    )
+    experiences: Optional[List[Experience]] = Field(
+        None, description="List of experiences of the Profile."
+    )
+    educations: Optional[List[Education]] = Field(
+        None, description="List of educations of the Profile."
+    )
+    attachments: List = Field(
+        ..., description="List of documents attached to the Profile."
+    )
+    skills: Optional[List[FieldSkill]] = Field(
+        None, description="List of skills of the Profile."
+    )
+    languages: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of spoken languages of the profile"
+    )
+    certifications: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of certifications of the Profile."
+    )
+    courses: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of courses of the Profile."
+    )
+    tasks: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of tasks of the Profile."
+    )
+    interests: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of interests of the Profile."
+    )
+    labels: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of labels of the Profile."
+    )
+    tags: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of tags of the Profile."
+    )
+    metadatas: Optional[List[HrflowProfileField]] = Field(
+        None, description="List of metadatas of the Profile."
+    )
