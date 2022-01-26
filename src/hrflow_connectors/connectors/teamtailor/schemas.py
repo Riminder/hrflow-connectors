5a7c5a609d60b7c923fa15c713225ba39dd13f0f
limamvadhel <limam.vadhel@student-cs.fr>
2022-01-26 08:48:43 +0100

    fix:  typing mistakes and typos in aliases in schemas

--- src/hrflow_connectors/connectors/teamtailor/schemas.py
+++ src/hrflow_connectors/connectors/teamtailor/schemas.py
@@ -4,30 +4,34 @@ from pydantic import BaseModel, Field
 
 class TeamtailorJobAttribute(BaseModel):
     title: str
-    pitch: str
+    pitch: Optional[str]
     body: str = Field(..., description="job description")
     created_at: str = Field(..., alias="created-at")
-    updated_at: str = Field(..., alias="updated-at")
-    status: str
+    updated_at: Optional[str] = Field(..., alias="updated-at")
+    status: Optional[str]
     tags: List[str]
-    remote_status: str = Field(..., alias="remote-status")
+    remote_status: Optional[str] = Field(..., alias="remote-status")
     currency: Optional[str]
-    salary_time_unit: Optional[str] = Field(..., alias="salary-time-unit")
-    min_salary: int = Field(..., alias="min-salary")
-    max_salary: int = Field(..., alias="max-salary")
-    employment_type: Optional[str] = Field(..., alias="employment-type")
-    employment_level: Optional[str] = Field(..., alias="employment-level")
+    salary_time_unit: Optional[str] = Field(None, alias="salary-time-unit")
+    min_salary: Optional[int] = Field(..., alias="min-salary")
+    max_salary: Optional[int] = Field(..., alias="max-salary")
+    employment_type: Optional[str] = Field(None, alias="employment-type")
+    employment_level: Optional[str] = Field(None, alias="employment-level")
     internal: Optional[bool]
-    start_date: Optional[str] = Field(..., alias="start-date")
-    end_date: Optional[str] = Field(..., alias="end-date")
+    start_date: Optional[str] = Field(None, alias="start-date")
+    end_date: Optional[str] = Field(None, alias="end-date")
+
+class TeamtailorJob(BaseModel):
+    attributes: TeamtailorJobAttribute
+
 
 
 class TeamtailorCandidateAttribute(BaseModel):
     first_name: str = Field(..., alias="first-name")
-    last_name: str = Field(..., alias="last_name")
+    last_name: str = Field(..., alias="last-name")
     email: str
     phone: Optional[str]
-    pitch: Optional[str] = Field(..., description="summary")
-    resume: Optional[str] = Field(..., description="candidate profile resume")
+    pitch: Optional[str] = Field(None, description="summary")
+    resume: Optional[str] = Field(None, description="candidate profile resume")
     sourced: Optional[bool]
-    tags: List
+    tags: Optional[List]
