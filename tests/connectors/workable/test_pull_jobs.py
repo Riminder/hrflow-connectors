592149aba44ced8ed09aaf60afccba79ce5cdb83
limamvadhel <limam.vadhel@student-cs.fr>
2022-01-26 14:10:06 +0100

    test: migrate board_keys to new boards in dev-demo

--- tests/connectors/workable/test_pull_jobs.py
+++ tests/connectors/workable/test_pull_jobs.py
@@ -5,6 +5,6 @@ def test_PullJobsAcrion(logger, hrflow_client):
     Workable.pull_jobs(
         subdomain="eurostar",
         hrflow_client=hrflow_client("dev-demo"),
-        board_key="12dc2abb0d952bfcbbc6259b76c0631754459787",
+        board_key="0d9a50e90e529e43394cf84b2f0666432551980b",
         hydrate_with_parsing=True,
     )
