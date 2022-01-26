2dca68840f69f92251f75635f58355b2a26956f2
clement <clement.negre@riminder.net>
2022-01-26 17:07:24 +0100

    doc: add docstring

--- src/hrflow_connectors/connectors/bullhorn/connector.py
+++ src/hrflow_connectors/connectors/bullhorn/connector.py
@@ -15,6 +15,24 @@ class Bullhorn(Connector):
                  profile: Profile,
                  **kwargs
             ) -> Optional[Dict[str, Any]]:
+        """
+        `PushProfileAction` pushes a Hrflow.ai profile to `Bullhorn`.
+
+        `Hrflow.ai` -> `Bullhorn`
+
+        Args:
+            hrflow_client (Hrflow): Hrflow client instance used to communicate with the Hrflow.ai API
+            auth (OAuth2Session): Auth instance to identify and communicate with the platform
+            profile (Profile): Profile to push
+            subdomain (str): Subdomain Bullhorn just before `bullhorn.com`. For example subdomain=`my_subdomain.my` in `http//my_subdomain.my.salesforce.com/ABC`
+            logics (List[str], optional): Function names to apply as filter before pushing the data. Default value `[]`
+            global_scope (Optional[Dict[str, Any]], optional): A dictionary containing the current scope's global variables. Default value `None`
+            local_scope (Optional[Dict[str, Any]], optional): A dictionary containing the current scope's local variables. Default value `None`
+            format_function_name (Optional[str], optional): Function name to format job before pushing. Default value `None`
+
+        Returns:
+            Optional[Dict[str, Any]]: Workflow response or `None`
+        """
         action = PushProfileAction(
             auth=auth,
             hrflow_client=hrflow_client,
