78b19da0133334bb4755716e1c206e03aaaa8bd7
Corentin Duchêne <corentin.duchene@riminder.net>
2022-01-27 17:20:14 +0100

    feat: use `Config` for Taleez

--- tests/connectors/taleez/test_push_profile.py
+++ tests/connectors/taleez/test_push_profile.py
@@ -4,14 +4,10 @@ from hrflow_connectors import XTaleezAuth
 from hrflow_connectors import Taleez
 from hrflow_connectors.utils.hrflow import Profile, Source
 
-@pytest.fixture
-def auth(credentials):
-    auth = XTaleezAuth(
-        name = 'X-taleez-api-secret',
-        value=credentials["taleez"]["X-taleez-api-secret"]
-    )
-    return auth
 
+@pytest.fixture
+def auth(config):
+    return XTaleezAuth(value=config.TALEEZ_TOKEN)
 
 
 def test_PushProfile(logger, auth, hrflow_client):
@@ -21,9 +17,9 @@ def test_PushProfile(logger, auth, hrflow_client):
         source=Source(key="762d2f25b855f7cfd13e5585ef727d8fb6e752cb"),
     )
     response = Taleez.push_profile(
-        job_id = None,
+        job_id=None,
         auth=auth,
-        recruiter_id = 15886,
+        recruiter_id=15886,
         hrflow_client=hrflow_client(),
         profile=profile,
     )
