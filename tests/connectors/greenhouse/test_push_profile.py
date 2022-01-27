0043f301168d4d60b54ace347d2c2482a9233691
Corentin Duchêne <corentin.duchene@riminder.net>
2022-01-27 16:48:01 +0100

    feat: use `Config` for Greenhouse

--- tests/connectors/greenhouse/test_push_profile.py
+++ tests/connectors/greenhouse/test_push_profile.py
@@ -5,16 +5,9 @@ from hrflow_connectors.core.auth import AuthorizationAuth, OAuth2PasswordCredent
 from hrflow_connectors.utils.hrflow import Profile, Source
 
 
-
-
 @pytest.fixture
-def auth(credentials):
-    auth = AuthorizationAuth(
-        name = 'Authorization',
-        value= credentials["greenhouse"]["oauth2"]["Authorization"]
-    )
-    
-    return auth
+def auth(config):
+    return AuthorizationAuth(value=config.GREENHOUSE_TOKEN)
 
 
 def test_PushProfile(logger, auth, hrflow_client):
@@ -24,8 +17,8 @@ def test_PushProfile(logger, auth, hrflow_client):
         source=Source(key="762d2f25b855f7cfd13e5585ef727d8fb6e752cb"),
     )
     response = Greenhouse.push_profile(
-        job_id = ["4179714004"] ,
-        on_behalf_of = "4249018004",
+        job_id=["4179714004"],
+        on_behalf_of="4249018004",
         auth=auth,
         hrflow_client=hrflow_client(),
         profile=profile,
