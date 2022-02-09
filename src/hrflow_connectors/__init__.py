a8a3e73b4afcc6947efaa070ac856143928a59f9
Jamal Gourinda (Remote Server) <jamal.gourinda@riminder.net>
2022-02-09 08:41:57 +0000

    major: Update poetry packaging information, update coding style, configure pre-commit hook, update core logic to add more metadata, add automatic doc generation

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -1,6 +1,8 @@
 from hrflow_connectors.connectors.smartrecruiters import SmartRecruiters
 from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
+from hrflow_connectors.core.documentation import generate_docs  # noqa
 
+__version__ = "2.0.0"
 __CONNECTORS__ = [SmartRecruiters]
 
 # This makes sure that connector are in module namespace
