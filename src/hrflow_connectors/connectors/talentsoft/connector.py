f60a0fccef56bfb3e454db069155649decd365bf
Jamal Gourinda (Remote Server) <jamal.gourinda@riminder.net>
2022-04-07 10:03:57 +0000

    feat: Add Talentsoft Profile warehouse with read capability

---
+++ src/hrflow_connectors/connectors/talentsoft/connector.py
@@ -0,0 +1,9 @@
+from hrflow_connectors.core import Connector
+
+DESCRIPTION = "TalentSoft"
+TalentSoft = Connector(
+    name="TalentSoft",
+    description=DESCRIPTION,
+    url="https://www.smartrecruiters.com/",
+    actions=[],
+)
---
+++ src/hrflow_connectors/connectors/talentsoft/warehouse.py
@@ -0,0 +1,92 @@
+import json
+import typing as t
+from io import BytesIO
+from logging import LoggerAdapter
+from zipfile import ZipFile
+
+import requests
+from pydantic import BaseModel, Field
+
+from hrflow_connectors.core import Warehouse, WarehouseReadAction
+
+GRANT_TYPE = "client_credentials"
+
+
+class ReadProfileParameters(BaseModel):
+    client_id: str = Field(
+        ..., description="Client ID used to access TalentSoft API", repr=False
+    )
+    client_secret: str = Field(
+        ..., description="Client Secret used to access TalentSoft API", repr=False
+    )
+    client_url: str = Field(..., description="URL of TalentSoft client integration")
+    token_scope: str = Field(
+        ..., description="Scope of the authentication token to request from TalentSoft "
+    )
+    reference: str = Field(
+        ..., description="TalentSoft reference of the profile to fetch"
+    )
+
+
+def read(
+    adapter: LoggerAdapter, parameters: ReadProfileParameters
+) -> t.Iterable[t.Dict]:
+    response = requests.post(
+        "{}/api/token".format(parameters.client_url),
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data=dict(
+            grant_type=GRANT_TYPE,
+            scope=parameters.token_scope,
+            client_id=parameters.client_id,
+            client_secret=parameters.client_secret,
+        ),
+    )
+    if not response.ok:
+        raise Exception(
+            "Failed to get authentication token with error={}".format(response.text)
+        )
+    try:
+        token = response.json()["access_token"]
+    except (KeyError, requests.exceptions.JSONDecodeError) as e:
+        raise Exception(
+            "Failed to get token from response with error={}".format(repr(e))
+        )
+
+    response = requests.get(
+        "{}/api/exports/v1/candidates?filter=id::{}".format(
+            parameters.client_url, parameters.reference
+        ),
+        headers={
+            "Authorization": "bearer {}".format(token),
+        },
+    )
+    if not response.ok:
+        raise Exception(
+            "Failed to fetch candidate with reference={} from TalentSoft with error={}"
+            .format(parameters.reference, response.text)
+        )
+    if not response.content:
+        adapter.warning(
+            "Empty content when fetching candidate with reference={} from TalentSoft"
+            .format(parameters.reference)
+        )
+        return
+
+    data = response.content
+    read_up_to = int.from_bytes(data[0:4], byteorder="little")
+    zip_data = ZipFile(BytesIO(data[4 : 4 + read_up_to]))
+    candidate = json.loads(zip_data.read("applicantdetail").decode())
+    for attachment in candidate["attachments"]:
+        attachment["raw"] = zip_data.read(attachment["id"])
+    yield candidate
+
+
+TalentSoftProfileWarehouse = Warehouse(
+    name="TalentSoft Profiles",
+    read=WarehouseReadAction(
+        parameters=ReadProfileParameters,
+        function=read,
+    ),
+)
