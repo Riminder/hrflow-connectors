490120309aa1e7a3654dca4cec7f21eaa43712e0
Jamal Gourinda (Remote Server) <jamal.gourinda@riminder.net>
2022-04-11 13:07:13 +0000

    feat: Add new write Profile and write Profile parsing capabilities for HrFlow warehoues

--- src/hrflow_connectors/connectors/hrflow/schemas.py
+++ src/hrflow_connectors/connectors/hrflow/schemas.py
@@ -273,3 +273,24 @@ class HrFlowProfile(BaseModel):
     metadatas: t.Optional[t.List[GeneralEntitySchema]] = Field(
         None, description="List of metadatas of the Profile."
     )
+
+
+class ResumeToParse(BaseModel):
+    raw: bytes
+    content_type: str
+
+
+class HrFlowProfileParsing(BaseModel):
+    reference: t.Optional[str] = Field(
+        ..., description="Custom identifier of the Profile."
+    )
+    created_at: str = Field(
+        ..., description="type: datetime ISO8601, Creation date of the Profile."
+    )
+    resume: ResumeToParse
+    tags: t.List[GeneralEntitySchema] = Field(
+        ..., description="List of tags of the Profile."
+    )
+    metadatas: t.List[GeneralEntitySchema] = Field(
+        ..., description="List of metadatas of the Profile."
+    )
