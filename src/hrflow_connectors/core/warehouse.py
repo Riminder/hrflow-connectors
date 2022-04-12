21390e459291815cf4ac61ba1992d6d1fee72cf9
Jamal Gourinda (Remote Server) <jamal.gourinda@riminder.net>
2022-04-12 10:45:41 +0000

    feat: Add method to fix some parameters of read/write functions

--- src/hrflow_connectors/core/warehouse.py
+++ src/hrflow_connectors/core/warehouse.py
@@ -1,7 +1,21 @@
+import enum
 import typing as t
 from logging import LoggerAdapter
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError, create_model
+
+
+class FieldNotFoundError(RuntimeError):
+    pass
+
+
+class FixedValueValidationError(RuntimeError):
+    pass
+
+
+class ActionType(enum.Enum):
+    read = enum.auto()
+    write = enum.auto()
 
 
 class ActionEndpoints(BaseModel):
@@ -41,3 +55,66 @@ class Warehouse(BaseModel):
     @property
     def is_writable(self):
         return self.write is not None
+
+    def with_fixed_read_parameters(self, **tofix) -> "Warehouse":
+        return self.__with_fixed_parameters(action_type=ActionType.read, **tofix)
+
+    def with_fixed_write_parameters(self, **tofix) -> "Warehouse":
+        return self.__with_fixed_parameters(action_type=ActionType.write, **tofix)
+
+    def __with_fixed_parameters(self, action_type: ActionType, **tofix) -> "Warehouse":
+        action_to_fix = getattr(self, action_type.name)
+        fixed = dict()
+        original_fields = action_to_fix.parameters.__fields__
+        for field, value in tofix.items():
+            if field not in original_fields:
+                raise FieldNotFoundError(
+                    "The field you are trying to fix '{}' is not part of the available"
+                    " parameters {}".format(field, list(original_fields.keys()))
+                )
+            try:
+                action_to_fix.parameters(**{field: value})
+            except ValidationError as e:
+                errors = e.errors()
+                field_error = next(
+                    (error for error in errors if error["loc"] == (field,)), None
+                )
+                if field_error is not None:
+                    raise FixedValueValidationError(
+                        "The value='{}' you are trying to use for field='{}' does not"
+                        " pass the original validation with error={}".format(
+                            value, field, field_error
+                        )
+                    )
+            original = action_to_fix.parameters.__fields__[field]
+            fixed[field] = (
+                original.type_,
+                Field(value, const=True, description=original.field_info.description),
+            )
+        with_fixed_parameters = create_model(
+            "Fixed{}Parameters".format(action_type.name.capitalize()),
+            __base__=action_to_fix.parameters,
+            **fixed
+        )
+        if action_type is ActionType.read:
+            return Warehouse(
+                name=self.name,
+                data_schema=self.data_schema,
+                read=WarehouseReadAction(
+                    endpoints=self.read.endpoints,
+                    parameters=with_fixed_parameters,
+                    function=self.read.function,
+                ),
+                write=self.write,
+            )
+
+        return Warehouse(
+            name=self.name,
+            data_schema=self.data_schema,
+            read=self.read,
+            write=WarehouseWriteAction(
+                endpoints=self.write.endpoints,
+                parameters=with_fixed_parameters,
+                function=self.write.function,
+            ),
+        )
