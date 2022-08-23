857add002f996fd1160e0cfa447c5213a75ce6d6
Jamal Gourinda (Remote Server) <jamal.gourinda@riminder.net>
2022-08-23 12:01:28 +0000

    feat: Store run results in backend when configured

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -1,5 +1,6 @@
 from hrflow_connectors.connectors.smartrecruiters import SmartRecruiters
 from hrflow_connectors.connectors.talentsoft import TalentSoft
+from hrflow_connectors.core import backend
 from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
 from hrflow_connectors.core.documentation import generate_docs  # noqa
 
@@ -10,3 +11,5 @@ __CONNECTORS__ = [SmartRecruiters, TalentSoft]
 # and that the automatic workflow code generation should work
 for connector in __CONNECTORS__:
     globals()[connector.model.name] = connector
+
+backend.configure_store()
