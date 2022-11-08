8a64528cbed0c5af9ff9bf1bde648c18f4ec5703
Nedhir Ebnou <nebnou@gmail.com>
2022-11-08 14:27:32 +0100

    Waalaxy Connector v1.0

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -1,11 +1,12 @@
 from hrflow_connectors.connectors.smartrecruiters import SmartRecruiters
 from hrflow_connectors.connectors.talentsoft import TalentSoft
+from hrflow_connectors.connectors.waalaxy import Waalaxy
 from hrflow_connectors.core import backend
 from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
 from hrflow_connectors.core.documentation import generate_docs  # noqa
 
 __version__ = "2.0.0"
-__CONNECTORS__ = [SmartRecruiters, TalentSoft]
+__CONNECTORS__ = [SmartRecruiters, TalentSoft, Waalaxy]
 
 # This makes sure that connector are in module namespace
 # and that the automatic workflow code generation should work
