ceca0097f4ad061a879436905c6071ee8993af40
Arthur Stievenard <stievenardarthur@gmail.com>
2023-01-12 15:45:11 +0100

    🎉 Connector Greenhouse
    * greenhouse-connector-final-version
    
    save
    
    connecotr warehouse updated
    
    greenhouse-connector-final-version
    
    * field_type added to read/write param
    
    * doc correction
    
    Co-authored-by: Moustapha Ebnou <57711045+riquelme222@users.noreply.github.com>

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -1,3 +1,4 @@
+from hrflow_connectors.connectors.greenhouse.connector import Greenhouse
 from hrflow_connectors.connectors.ceridian import Ceridian
 from hrflow_connectors.connectors.bullhorn import Bullhorn
 from hrflow_connectors.connectors.sapsuccessfactors import SAPSuccessFactors
@@ -13,7 +14,7 @@ from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
 from hrflow_connectors.core.documentation import generate_docs  # noqa
 
 __version__ = "2.0.0"
-__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian]
+__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian, Greenhouse]
 
 # This makes sure that connector are in module namespace
 # and that the automatic workflow code generation should work
