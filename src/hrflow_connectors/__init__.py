dd755a1191501de418340ad57edc74aa269bdcf3
Benchovski <46778695+DanielRosa73@users.noreply.github.com>
2023-01-12 15:51:42 +0100

    🎉 Teamtailor connector
    * [SQUASH] ready for pull request (added field_type)
    
    [SQUASH] removed is_auth field
    
    This is a combination of 6 commits.
    
    [ADD] init, schema, warehouse and connector
    
    [MOD] connector and warehouse
    
    [ADD] test-config.yaml
    
    [MOD] added comments, removed test print()'s
    
    [MOD] style
    
    [ADD] __init__.py
    
    [SQUASH] ready for pull-request
    
    [ADD] docs and README
    
    [MOD] manifest.json
    
    [MOD] added is_auth field to read and write parameters
    
    [MOD] manifest.json
    
    [MOD] Style
    
    [MOD] removed is_auth field from write param
    
    [MOD] manifest and docs
    
    core: Define custom ParametersModel to force declaration of field_type
    
    feat: Update tests and add tests for field_type validation
    
    feat: Update existing warehouses with field_type
    
    feat: Update manifest.json with new field_type
    
    feat: Update DOCUMENTATION.md to reflect new ParametersModel and field_type constraints
    
    added new field_auth type, and query filters for read action
    
    * [ADD] docs
    
    * Applied review advices
    
    * Create DOCUMENTATION.md
    
    * __nint__
    
    Co-authored-by: Daniel Rosa <daniel.rosa@hrflow.ai>
    Co-authored-by: Moustapha Ebnou <57711045+riquelme222@users.noreply.github.com>

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -8,13 +8,14 @@ from hrflow_connectors.connectors.poleemploi import PoleEmploi
 from hrflow_connectors.connectors.recruitee import Recruitee
 from hrflow_connectors.connectors.smartrecruiters import SmartRecruiters
 from hrflow_connectors.connectors.talentsoft import TalentSoft
+from hrflow_connectors.connectors.teamtailor import Teamtailor
 from hrflow_connectors.connectors.workable import Workable
 from hrflow_connectors.core import backend
 from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
 from hrflow_connectors.core.documentation import generate_docs  # noqa
 
 __version__ = "2.0.0"
-__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian, Greenhouse]
+__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian, Greenhouse, Teamtailor]
 
 # This makes sure that connector are in module namespace
 # and that the automatic workflow code generation should work
