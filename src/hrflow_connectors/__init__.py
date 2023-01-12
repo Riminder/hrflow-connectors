4dcc7c7bc79f3d3506bd4e134c0c5b903be76afc
Mohamed Nedhirou EBNOU <107410704+nedhirouebnou@users.noreply.github.com>
2023-01-12 16:06:03 +0100

    🎉 Hubspot Connector : convert your HrFlow.ai Profiles to contacts/leads in Hubspot
    * Hubspot Connector v2.0
    
    * Pull request review updates
    
    Co-authored-by: Nedhir Ebnou <nebnou@gmail.com>
    Co-authored-by: Moustapha Ebnou <57711045+riquelme222@users.noreply.github.com>

--- src/hrflow_connectors/__init__.py
+++ src/hrflow_connectors/__init__.py
@@ -1,3 +1,4 @@
+from hrflow_connectors.connectors.hubspot import Hubspot
 from hrflow_connectors.connectors.greenhouse.connector import Greenhouse
 from hrflow_connectors.connectors.ceridian import Ceridian
 from hrflow_connectors.connectors.bullhorn import Bullhorn
@@ -17,7 +18,7 @@ from hrflow_connectors.core.connector import hrflow_connectors_manifest  # noqa
 from hrflow_connectors.core.documentation import generate_docs  # noqa
 
 __version__ = "2.0.0"
-__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian, Greenhouse, Teamtailor, Waalaxy]
+__CONNECTORS__ = [SmartRecruiters, TalentSoft, PoleEmploi, Adzuna, Recruitee, Workable, BreezyHR, SAPSuccessFactors, Bullhorn, Ceridian, Greenhouse, Teamtailor, Waalaxy, Hubspot]
 
 # This makes sure that connector are in module namespace
 # and that the automatic workflow code generation should work
