dc2cd6e2e404610ef16aa86739df66b0dd2fb395
the-forest-tree <the-forest-tree@hrflow.ai>
2023-07-31 14:12:08 +0000

    feat: Complete Salesforce pull_profile_list action with attachment and ome hedge cases management

--- src/hrflow_connectors/connectors/salesforce/connector.py
+++ src/hrflow_connectors/connectors/salesforce/connector.py
@@ -1,13 +1,10 @@
-import base64
 import json
 import typing as t
+from datetime import datetime
 
-import requests
-
+from hrflow_connectors.connectors.hrflow.warehouse.profile import HrFlowProfileWarehouse
 from hrflow_connectors.connectors.salesforce.schemas import SalesforceHrFlowProfile
 from hrflow_connectors.connectors.salesforce.warehouse import SalesforceProfileWarehouse
-from hrflow_connectors.connectors.hrflow.schemas import HrFlowProfile
-from hrflow_connectors.connectors.hrflow.warehouse.profile import HrFlowProfileWarehouse
 from hrflow_connectors.core import (
     ActionName,
     ActionType,
@@ -18,195 +15,99 @@ from hrflow_connectors.core import (
 )
 
 
-def to_int(elm: t.Any) -> int:
-    if elm is None:
-        return 0
-    return int(elm)
-
-
-def get_location(info: t.Dict) -> t.Dict:
-    if info is not None:
-        location = info.get("location")
-        if location is None:
-            location = dict()
-        fields = location.get("fields", {})
-        if fields == []:
-            fields = {}
-        location_dict = {
-            "address1": location.get("text"),
-            "city": fields.get("city"),
-            "state": fields.get("country"),
-            "zip": fields.get("postcode"),
-        }
-        return location_dict
-    return None
-
-
-def get_skills(data: t.Dict) -> str:
-    skills = ""
-    if data.get("skills") is not None:
-        for i in range(len(data["skills"]) - 1):
-            skills += data["skills"][i]["name"] + ", "
-        skills += data["skills"][-1]["name"]
-    return skills
-
-
-def get_education(education_list: t.List[t.Dict]) -> t.List[t.Dict]:
-    educations = []
-    for hrflow_education in education_list:
-        location = hrflow_education["location"]
-        education = {
-            "id": "0",
-            "candidate": {"id": None},
-            "school": hrflow_education.get("school"),
-            "degree": hrflow_education.get("title"),
-            "comments": hrflow_education.get("description"),
-            "city": location.get("text") if location else None,
-            "startDate": int(
-                date_format.from_str_to_datetime(
-                    hrflow_education.get("date_start")
-                ).timestamp()
-            )
-            if hrflow_education.get("date_start")
-            else None,
-            "endDate": int(
-                date_format.from_str_to_datetime(
-                    hrflow_education.get("date_end")
-                ).timestamp()
-            )
-            if hrflow_education.get("date_start")
-            else None,
-        }
-        educations.append(education)
-    return educations
-
-
-def get_experience(experience_list: t.List[t.Dict]) -> t.List[t.Dict]:
-    experience_json = []
-    for hrflow_experience in experience_list:
-        experience = {
-            "id": "0",
-            "candidate": {"id": None},
-            "companyName": hrflow_experience.get("company"),
-            "title": hrflow_experience.get("title"),
-            "comments": hrflow_experience.get("description"),
-            "startDate": int(
-                date_format.from_str_to_datetime(
-                    hrflow_experience.get("date_start")
-                ).timestamp()
-            )
-            if hrflow_experience.get("date_start")
-            else None,
-            "endDate": int(
-                date_format.from_str_to_datetime(
-                    hrflow_experience.get("date_end")
-                ).timestamp()
-            )
-            if hrflow_experience.get("date_end")
-            else None,
-        }
-        experience_json.append(experience)
-    return experience_json
-
-
-def get_attachments(attachment_list: t.List[t.Dict]) -> t.List[t.Dict]:
-    attachments_json = []
-    for hrflow_attachment in attachment_list:
-        url = hrflow_attachment["public_url"]
-        response = requests.get(url)
-        b64 = base64.b64encode(response.content)
-
-        attachment = {
-            "externalID": "portfolio",
-            "fileContent": b64.decode(),
-            "fileType": "SAMPLE",
-            "name": hrflow_attachment["file_name"],
-            "contentType": "text/plain",
-            "description": "Resume file for candidate.",
-            "type": "cover",
-        }
-        attachments_json.append(attachment)
-    return attachments_json
-
-
 def format_profile(data: SalesforceHrFlowProfile) -> t.Dict:
     info = dict(
-        full_name=f"{data.Last_Name__c} {data.First_Name__c}",
-        first_name=data.First_Name__c,
-        last_name=data.Last_Name__c,
-        email=data.Email__c,
-        phone=data.Phone__c,
-        date_birth=data.Date_Birth__c,
+        full_name=f"{data['Last_Name__c']} {data['First_Name__c']}",
+        first_name=data["First_Name__c"],
+        last_name=data["Last_Name__c"],
+        email=data["Email__c"],
+        phone=data["Phone__c"],
+        date_birth=data["Date_Birth__c"],
         location=dict(
-            text=data.Location_Text__c, 
-            lat=data.Location_Lat__c,
-            lng=data.Location_Lng__c,
-            fields=json.loads(data.Location_Fields__c)
+            text=data["Location_Text__c"],
+            lat=data["Location_Lat__c"],
+            lng=data["Location_Lng__c"],
         ),
-        gender=data.Gender__c,
+        gender=data["Gender__c"],
     )
-    experiences = [
-        dict(
-            title=experience.Title__c,
-            location=dict(
-                text=experience.Location_Text__c, 
-                lat=experience.Location_Lat__c,
-                lng=experience.Location_Lng__c,
-                fields=json.loads(experience.Location_Fields__c)
-            ),
-            company=experience.Company__c,
-            date_start=experience.Date_Begin__c,
-            date_end=experience.Date_End__c,
-            description=experience.Description__c,
-            skills=json.loads(experience.Skills__c),
-            tasks=json.loads(experience.Tasks__c),
-            certifications=json.loads(experience.Certifications__c),
-        )
-        for experience in data.HrFlow_Profile_Experiences__r.records
-    ]
-    educations = [
-        dict(
-            title=education.Title__c,
-            location=dict(
-                text=education.Location_Text__c, 
-                lat=education.Location_Lat__c,
-                lng=education.Location_Lng__c,
-                fields=json.loads(education.Location_Fields__c)
-            ),
-            school=education.School__c,
-            date_start=education.Date_Begin__c,
-            date_end=education.Date_End__c,
-            description=education.Description__c,
-            skills=json.loads(education.Skills__c),
-            tasks=json.loads(education.Tasks__c),
-            certifications=json.loads(education.Certifications__c),
-            courses=json.loads(education.Courses__c),
-        )
-        for education in data.HrFlow_Profile_Educations__r.records
-    ]
+    experiences = []
+    if data["HrFlow_Profile_Experiences__r"]:
+        experiences = [
+            dict(
+                title=experience["Title__c"],
+                location=dict(
+                    text=experience["Location_Text__c"],
+                    lat=experience["Location_Lat__c"],
+                    lng=experience["Location_Lng__c"],
+                ),
+                company=experience["Company__c"],
+                date_start=dict(iso8601=experience["Date_Begin__c"]),
+                date_end=dict(iso8601=experience["Date_End__c"]),
+                description=experience["Description__c"],
+                skills=json.loads(experience["Skills__c"]),
+                tasks=json.loads(experience["Tasks__c"]),
+                certifications=json.loads(experience["Certifications__c"]),
+            )
+            for experience in data["HrFlow_Profile_Experiences__r"]["records"]
+        ]
+    educations = []
+    if data["HrFlow_Profile_Educations__r"]:
+        educations = [
+            dict(
+                title=education["Title__c"],
+                location=dict(
+                    text=education["Location_Text__c"],
+                    lat=education["Location_Lat__c"],
+                    lng=education["Location_Lng__c"],
+                ),
+                school=education["School__c"],
+                date_start=dict(iso8601=education["Date_Begin__c"]),
+                date_end=dict(iso8601=education["Date_End__c"]),
+                description=education["Description__c"],
+                skills=json.loads(education["Skills__c"]),
+                tasks=json.loads(education["Tasks__c"]),
+                certifications=json.loads(education["Certifications__c"]),
+                courses=json.loads(education["Courses__c"]),
+            )
+            for education in data["HrFlow_Profile_Educations__r"]["records"]
+        ]
+    attachments = []
+    if data["HrFlow_Profile_Attachments__r"]:
+        attachments = [
+            dict(
+                text=attachment["Text__c"],
+                type=attachment["Type__c"],
+                alt=attachment["Alt__c"],
+                file_size=attachment["File_Size__c"],
+                file_name=attachment["File_Name__c"],
+                original_file_name=attachment["Original_File_Name__c"],
+                extension=attachment["Extension__c"],
+                url=attachment["URL__c"],
+            )
+            for attachment in data["HrFlow_Profile_Attachments__r"]["records"]
+        ]
     return dict(
         key=data["Hash_Id__c"],
         reference=data["Reference__c"],
-        archived_at=data.Archive__c,
-        updated_at=data.Date_Edition__c,
-        created_at=data.Timestamp__c,
+        archived_at=data["Archive__c"],
+        updated_at=data["Date_Edition__c"],
+        created_at=data["Timestamp__c"],
         info=info,
-        text_language=data.Text_Language__c,
-        text=data.Text__c,
-        educations_duration=data.Experiences_Duration__c,
+        text_language=data["Text_Language__c"],
+        text=data["Text__c"],
+        educations_duration=data["Experiences_Duration__c"],
         experiences=experiences,
         educations=educations,
-        # FIXME ME add attachments
-        attachments=None,
-        skills=json.loads(data.Skills__c),
-        languages=json.loads(data.Languages__c),
-        certifications=json.loads(data.Certifications__c),
-        courses=json.loads(data.Courses__c),
-        tasks=json.loads(data.Tasks__c),
-        interests=json.loads(data.Interests__c),
-        labels=json.loads(data.Labels__c),
-        tags=json.loads(data.Tags__c),
-        metadatas=json.loads(data.Metadatas__c),
+        attachments=attachments,
+        skills=json.loads(data["Skills__c"]),
+        languages=json.loads(data["Languages__c"]),
+        certifications=json.loads(data["Certifications__c"]),
+        courses=json.loads(data["Courses__c"]),
+        tasks=json.loads(data["Tasks__c"]),
+        interests=json.loads(data["Interests__c"]),
+        labels=json.loads(data["Labels__c"]),
+        tags=json.loads(data["Tags__c"]),
+        metadatas=json.loads(data["Metadatas__c"]),
     )
 
 
@@ -226,7 +127,8 @@ Salesforce = Connector(
             name=ActionName.pull_profile_list,
             trigger_type=WorkflowType.pull,
             description=(
-                "Retrieves profiles from Salesforce HrFlow Custom Objects and writes them to Hrflow.ai source"
+                "Retrieves profiles from Salesforce HrFlow Profile & Co Custom Objects"
+                " and writes them to an Hrflow.ai source"
             ),
             parameters=BaseActionParameters.with_defaults(
                 "ReadProfileActionParameters", format=format_profile
--- src/hrflow_connectors/connectors/salesforce/warehouse.py
+++ src/hrflow_connectors/connectors/salesforce/warehouse.py
@@ -1,15 +1,12 @@
+import json
 import typing as t
+from datetime import datetime, timezone
 from logging import LoggerAdapter
-from datetime import date
 
 from pydantic import Field
 from simple_salesforce import Salesforce
 
-
-
-from hrflow_connectors.connectors.salesforce.schemas import (
-    SalesforceHrFlowProfile,
-)
+from hrflow_connectors.connectors.salesforce.schemas import SalesforceHrFlowProfile
 from hrflow_connectors.core import (
     DataType,
     FieldType,
@@ -43,11 +40,12 @@ SELECT
     )
 FROM HrFlow_Profile__c
 WHERE LastModifiedDate >= {last_modified_date}
+ORDER BY LastModifiedDate, Id__c
 LIMIT {limit}
 OFFSET {offset}
 """
 MAX_SOQL_LIMIT = 200
-MAX_PROFILES = 1000
+MAX_PROFILES = 500
 
 
 class ReadProfilesParameters(ParametersModel):
@@ -65,19 +63,23 @@ class ReadProfilesParameters(ParametersModel):
     )
     sf_security_token: str = Field(
         ...,
-        description="Security Token to access Salesforce API." 
-            "See below for instructions: "
-            "How Can I Find My Security Token and Use It in Data Loader | Salesforce Platform "
-            " https://www.youtube.com/watch?v=nYbfxeSGKFM&ab_channel=SalesforceSupport",
+        description=(
+            "Security Token to access Salesforce API.See below for instructions: How"
+            " Can I Find My Security Token and Use It in Data Loader | Salesforce"
+            " Platform "
+            " https://www.youtube.com/watch?v=nYbfxeSGKFM&ab_channel=SalesforceSupport"
+        ),
         repr=False,
         field_type=FieldType.Auth,
     )
     sf_organization_id: str = Field(
         ...,
-        description="Security Token to access Salesforce API." 
+        description=(
+            "Security Token to access Salesforce API."
             "See below for instructions: "
             "How to find your organization id "
-            " https://help.salesforce.com/s/articleView?id=000385215&type=1",
+            " https://help.salesforce.com/s/articleView?id=000385215&type=1"
+        ),
         repr=False,
         field_type=FieldType.Auth,
     )
@@ -104,18 +106,39 @@ def read(
     read_from: t.Optional[str] = None,
 ) -> t.Iterable[t.Dict]:
     sf = Salesforce(
-        username=parameters.sf_username, 
-        password=parameters.sf_password, 
-        security_token=parameters.sf_security_token, 
-        organizationId=parameters.sf_organization_id
+        username=parameters.sf_username,
+        password=parameters.sf_password,
+        security_token=parameters.sf_security_token,
+        organizationId=parameters.sf_organization_id,
     )
     if read_mode is ReadMode.sync:
         if parameters.last_modified_date is None:
             raise Exception("last_modified_date cannot be None in ReadMode.sync")
         last_modified_date = parameters.last_modified_date
     else:
-        last_modified_date = read_from or date.today().isoformat()
-    
+        if parameters.last_modified_date is not None:
+            raise Exception(
+                "last_modified_date should not be supplied in ReadMode.incremental"
+            )
+        if read_from:
+            try:
+                read_from = json.loads(read_from)
+                last_modified_date = read_from["last_modified_date"]
+                last_id = read_from["hrflow_id"]
+            except json.JSONDecodeError as e:
+                raise Exception(f"Failed to JSON parse read_from={read_from} error={e}")
+            except KeyError as e:
+                raise Exception(
+                    "Failed to find expected key in"
+                    f" read_from={read_from} error={repr(e)}"
+                )
+        else:
+            last_modified_date = (
+                datetime.now(tz=timezone.utc)
+                .replace(hour=0, minute=0, second=0, microsecond=0)
+                .isoformat()
+            )
+            last_id = 0
 
     offset = 0
     profiles_to_pull = min(MAX_PROFILES, parameters.limit)
@@ -125,14 +148,23 @@ def read(
         query = SELECT_PROFILES_SOQL.format(
             last_modified_date=last_modified_date,
             limit=limit,
-            offset=offset
+            offset=offset,
         )
         profiles = sf.query_all(query)["records"]
+        if len(profiles) == 0:
+            break
+        new_profiles = 0
         for profile in profiles:
+            if (
+                profile["LastModifiedDate"] == last_modified_date
+                and profile["Id__c"] <= last_id
+            ):
+                continue
+            new_profiles += 1
             yield profile
 
         offset += len(profiles)
-        profiles_to_pull -= offset
+        profiles_to_pull -= new_profiles
         limit = min(MAX_SOQL_LIMIT, profiles_to_pull)
 
 
@@ -143,5 +175,12 @@ SalesforceProfileWarehouse = Warehouse(
     read=WarehouseReadAction(
         parameters=ReadProfilesParameters,
         function=read,
+        supports_incremental=True,
+        item_to_read_from=lambda profile: json.dumps(
+            dict(
+                last_modified_date=profile["LastModifiedDate"],
+                hrflow_id=profile["Id__c"],
+            )
+        ),
     ),
 )
