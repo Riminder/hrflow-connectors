4be70ddde95f460a69d566c3d1ccaf26cce8fb5c
the-forest-tree <the-forest-tree@hrflow.ai>
2023-08-07 15:47:51 +0000

    feat: Add Salesforce.push_profile action

--- src/hrflow_connectors/connectors/salesforce/connector.py
+++ src/hrflow_connectors/connectors/salesforce/connector.py
@@ -1,6 +1,7 @@
 import json
 import typing as t
 
+from hrflow_connectors.connectors.hrflow.schemas import HrFlowProfile
 from hrflow_connectors.connectors.hrflow.warehouse import (
     HrFlowJobWarehouse,
     HrFlowProfileWarehouse,
@@ -23,7 +24,7 @@ from hrflow_connectors.core import (
 )
 
 
-def format_profile(data: SalesforceHrFlowProfile) -> t.Dict:
+def format_into_hrflow_profile(data: SalesforceHrFlowProfile) -> t.Dict:
     info = dict(
         full_name=f"{data['Last_Name__c']} {data['First_Name__c']}",
         first_name=data["First_Name__c"],
@@ -119,6 +120,121 @@ def format_profile(data: SalesforceHrFlowProfile) -> t.Dict:
     )
 
 
+def format_into_salesforce_profile(data: HrFlowProfile) -> t.Dict:
+    experiences = None
+    if data["experiences"]:
+        experiences = dict(
+            done=True,
+            totalSize=len(data["experiences"]),
+            records=[
+                dict(
+                    Certifications__c=json.dumps(experience["certifications"]),
+                    Company__c=experience["company"],
+                    Courses__c=json.dumps(experience["courses"]),
+                    Date_Begin__c=experience["date_start"],
+                    Date_End__c=experience["date_end"],
+                    Description__c=experience["description"],
+                    Hash_Id__c=experience["key"],
+                    Location_Fields__c=json.dumps(experience["location"]["fields"]),
+                    Location_Lat__c=experience["location"]["lat"],
+                    Location_Lng__c=experience["location"]["lng"],
+                    Location_Text__c=experience["location"]["text"],
+                    Location_Gmaps__c=experience["location"]["gmaps"],
+                    Skills__c=json.dumps(experience["skills"]),
+                    Tasks__c=json.dumps(experience["tasks"]),
+                    Title__c=experience["title"],
+                )
+                for experience in data["experiences"]
+            ],
+        )
+    educations = None
+    if data["educations"]:
+        educations = dict(
+            done=True,
+            totalSize=len(data["educations"]),
+            records=[
+                dict(
+                    Certifications__c=json.dumps(education["certifications"]),
+                    School__c=education["school"],
+                    Courses__c=json.dumps(education["courses"]),
+                    Date_Begin__c=education["date_start"],
+                    Date_End__c=education["date_end"],
+                    Description__c=education["description"],
+                    Hash_Id__c=education["key"],
+                    Location_Fields__c=json.dumps(education["location"]["fields"]),
+                    Location_Lat__c=education["location"]["lat"],
+                    Location_Lng__c=education["location"]["lng"],
+                    Location_Text__c=education["location"]["text"],
+                    Location_Gmaps__c=education["location"]["gmaps"],
+                    Skills__c=json.dumps(education["skills"]),
+                    Tasks__c=json.dumps(education["tasks"]),
+                    Title__c=education["title"],
+                )
+                for education in data["educations"]
+            ],
+        )
+    attachments = None
+    if data["attachments"]:
+        attachments = dict(
+            done=True,
+            totalSize=len(data["attachments"]),
+            records=[
+                dict(
+                    Alt__c=attachment["alt"],
+                    Date_Edition__c=attachment["updated_at"],
+                    Extension__c=attachment["extension"],
+                    File_Name__c=attachment["file_name"],
+                    File_Size__c=attachment["file_size"],
+                    Original_File_Name__c=attachment["original_file_name"],
+                    Timestamp__c=attachment["created_at"],
+                    Type__c=attachment["type"],
+                    URL__c=attachment["public_url"],
+                )
+                for attachment in data["attachments"]
+            ],
+        )
+    return dict(
+        Id__c=data["id"],
+        Hash_Id__c=data["key"],
+        Reference__c=data["reference"],
+        Archive__c=data["archived_at"],
+        Date_Edition__c=data["updated_at"],
+        # Check if created_at works for both Date_Reception and Timestamp
+        Date_Reception__c=data["created_at"],
+        Timestamp__c=data["created_at"],
+        First_Name__c=data["info"]["first_name"],
+        Last_Name__c=data["info"]["last_name"],
+        Email__c=data["info"]["email"],
+        Phone__c=data["info"]["phone"],
+        Date_Birth__c=data["info"]["date_birth"],
+        Location_Fields__c=json.dumps(data["info"]["location"]["fields"]),
+        Location_Lat__c=data["info"]["location"]["lat"],
+        Location_Lng__c=data["info"]["location"]["lng"],
+        Location_Text__c=data["info"]["location"]["text"],
+        Location_Gmaps__c=data["info"]["location"]["gmaps"],
+        URLs__c=json.dumps(data["info"]["urls"]),
+        Picture__c=data["info"]["picture"],
+        Gender__c=data["info"]["gender"],
+        Summary__c=data["info"]["summary"],
+        Text_Language__c=data["text_language"],
+        Text__c=data["text"],
+        Experiences_Duration__c=data["experiences_duration"],
+        Educations_Duration__c=data["educations_duration"],
+        HrFlow_Profile_Experiences__r=experiences,
+        HrFlow_Profile_Educations__r=educations,
+        HrFlow_Profile_Attachments__r=attachments,
+        Skills__c=json.dumps(data["skills"]),
+        Languages__c=json.dumps(data["languages"]),
+        Certifications__c=json.dumps(data["certifications"]),
+        Courses__c=json.dumps(data["courses"]),
+        Tasks__c=json.dumps(data["tasks"]),
+        Interests__c=json.dumps(data["interests"]),
+        Labels__c=json.dumps(data["labels"]),
+        Tags__c=json.dumps(data["tags"]),
+        Metadatas__c=json.dumps(data["metadatas"]),
+    )
+
+
 def format_job(data: SalesforceHrFlowJob) -> t.Dict:
     return dict(
         archived_at=data["Archive__c"],
@@ -168,11 +284,25 @@ Salesforce = Connector(
                 " and writes them to an Hrflow.ai source"
             ),
             parameters=BaseActionParameters.with_defaults(
-                "ReadProfileActionParameters", format=format_profile
+                "ReadProfileActionParameters", format=format_into_hrflow_profile
             ),
             origin=SalesforceProfileWarehouse,
             target=HrFlowProfileWarehouse,
             action_type=ActionType.inbound,
+        ),
+        ConnectorAction(
+            name=ActionName.push_profile,
+            trigger_type=WorkflowType.catch,
+            description=(
+                "Pushs specific Profile from HrFlow and writes"
+                " it to HrFlow_Profile__c & Co Custom Objects in Salesforce"
+            ),
+            parameters=BaseActionParameters.with_defaults(
+                "PushProfileActionParameters", format=format_into_salesforce_profile
+            ),
+            origin=HrFlowProfileWarehouse,
+            target=SalesforceProfileWarehouse,
+            action_type=ActionType.outbound,
         ),
         ConnectorAction(
             name=ActionName.pull_job_list,
---
+++ src/hrflow_connectors/connectors/salesforce/docs/push_profile.md
@@ -0,0 +1,68 @@
+
+# Push profile
+`HrFlow.ai Profiles` :arrow_right: `Salesforce Profiles`
+
+Pushs specific Profile from HrFlow and writes it to HrFlow_Profile__c & Co Custom Objects in Salesforce
+
+
+
+## Action Parameters
+
+| Field | Type | Default | Description |
+| ----- | ---- | ------- | ----------- |
+| `logics`  | `typing.List[typing.Callable[[typing.Dict], typing.Optional[typing.Dict]]]` | [] | List of logic functions |
+| `format`  | `typing.Callable[[typing.Dict], typing.Dict]` | [`format_into_salesforce_profile`](../connector.py#L123) | Formatting function |
+| `read_mode`  | `str` | ReadMode.sync | If 'incremental' then `read_from` of the last run is given to Origin Warehouse during read. **The actual behavior depends on implementation of read**. In 'sync' mode `read_from` is neither fetched nor given to Origin Warehouse during read. |
+
+## Source Parameters
+
+| Field | Type | Default | Description |
+| ----- | ---- | ------- | ----------- |
+| `api_secret` :red_circle: | `str` | None | X-API-KEY used to access HrFlow.ai API |
+| `api_user` :red_circle: | `str` | None | X-USER-EMAIL used to access HrFlow.ai API |
+| `source_key` :red_circle: | `str` | None | HrFlow.ai source key |
+| `profile_key` :red_circle: | `str` | None | HrFlow.ai profile key |
+
+## Destination Parameters
+
+| Field | Type | Default | Description |
+| ----- | ---- | ------- | ----------- |
+| `sf_username` :red_circle: | `str` | None | username used to access Salesforce API |
+| `sf_password` :red_circle: | `str` | None | password used to access Salesforce API |
+| `sf_security_token` :red_circle: | `str` | None | Security Token to access Salesforce API.See below for instructions: How Can I Find My Security Token and Use It in Data Loader | Salesforce Platform  https://www.youtube.com/watch?v=nYbfxeSGKFM&ab_channel=SalesforceSupport |
+| `sf_organization_id` :red_circle: | `str` | None | Security Token to access Salesforce API.See below for instructions: How to find your organization id  https://help.salesforce.com/s/articleView?id=000385215&type=1 |
+
+:red_circle: : *required*
+
+## Example
+
+```python
+import logging
+from hrflow_connectors import Salesforce
+from hrflow_connectors.core import ReadMode
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+Salesforce.push_profile(
+    workflow_id="some_string_identifier",
+    action_parameters=dict(
+        logics=[],
+        format=lambda *args, **kwargs: None # Put your code logic here,
+        read_mode=ReadMode.sync,
+    ),
+    origin_parameters=dict(
+        api_secret="your_api_secret",
+        api_user="your_api_user",
+        source_key="your_source_key",
+        profile_key="your_profile_key",
+    ),
+    target_parameters=dict(
+        sf_username="your_sf_username",
+        sf_password="your_sf_password",
+        sf_security_token="your_sf_security_token",
+        sf_organization_id="your_sf_organization_id",
+    )
+)
+```
