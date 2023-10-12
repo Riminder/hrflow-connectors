a3c54ea990d6d77312b15b207bd4aa04749f84bf
nicolae-istratii <nicolae.istratii@hrflow.ai>
2023-10-12 16:16:03 +0200

    feature: added file name too long error

--- src/hrflow_connectors/connectors/workday/utils/errors.py
+++ src/hrflow_connectors/connectors/workday/utils/errors.py
@@ -6,3 +6,10 @@ class WorkdayNumberOutOfBoundsError(Exception):
         self, name: str, value: float, lb: float = -inf, ub: float = inf
     ) -> None:
         super().__init__(f"{name} must be between {lb} and {ub}, got {value}")
+
+
+class WorkdayFileNameTooLongError(Exception):
+    def __init__(self, name: str, max: int, len: int) -> None:
+        super().__init__(
+            f"{name} expected to be at most {max} characters long, got {len}"
+        )
