b70f898a27d63457bfd859b69480589206708066
Abdellahitech <abdellahi.mezid@hrflow.ai>
2024-01-08 09:24:15 +0100

    fix:add referential civlity

---
+++ src/hrflow_connectors/connectors/talentsoft/utils/const.py
@@ -0,0 +1,44 @@
+CIVILITY = [
+        {
+            "code": 3445,
+            "clientCode": "_TS_CO_Civility_Dr",
+            "label": "Dr.",
+            "type": "civility",
+            "parentType": "",
+        },
+        {
+            "code": 1790,
+            "clientCode": "_TS_CO_Civility_Miss",
+            "label": "Miss",
+            "type": "civility",
+            "parentType": "",
+        },
+        {
+            "code": 280,
+            "clientCode": "_TS_CO_Civility_Mr",
+            "label": "Mr.",
+            "type": "civility",
+            "parentType": "",
+        },
+        {
+            "code": 1789,
+            "clientCode": "_TS_CO_Civility_Mrs ",
+            "label": "Mrs",
+            "type": "civility",
+            "parentType": "",
+        },
+        {
+            "code": 281,
+            "clientCode": "_TS_CO_Civility_Mme",
+            "label": "Ms.",
+            "type": "civility",
+            "parentType": "",
+        },
+        {
+            "code": 3467,
+            "clientCode": "_TS_CO_Civility_Other",
+            "label": "Other",
+            "type": "civility",
+            "parentType": "",
+        }
+    ],
