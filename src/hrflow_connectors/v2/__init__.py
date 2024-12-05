2167dc995bdcd498ae7e769a3735b23f23688d50
Nedhir Ebnou <nebnou@gmail.com>
2024-12-05 12:27:50 +0100

    feature: Add France Travail(ex: Pole Emploi) Connector in CRUD v2

--- src/hrflow_connectors/v2/__init__.py
+++ src/hrflow_connectors/v2/__init__.py
@@ -1,6 +1,7 @@
 from hrflow_connectors.v2.connectors.admen import Admen
 from hrflow_connectors.v2.connectors.adzuna import Adzuna
 from hrflow_connectors.v2.connectors.bullhorn import Bullhorn
+from hrflow_connectors.v2.connectors.francetravail import FranceTravail
 from hrflow_connectors.v2.connectors.hubspot import Hubspot
 from hrflow_connectors.v2.connectors.recruitee import Recruitee
 from hrflow_connectors.v2.connectors.smartrecruiters import SmartRecruiters
@@ -20,4 +21,5 @@ __CONNECTORS__ = [
     Hubspot,
     Recruitee,
     Adzuna,
+    FranceTravail,
 ]
